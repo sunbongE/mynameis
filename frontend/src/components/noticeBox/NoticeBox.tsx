8232bb746155c568777ab52f6e15779c004395a9
조은서 <eundeok9@gmail.com>
2024-01-23 11:00:11 +0900

    S10P12C207-56 feat: noticeBox 개발
    noticeBox 구현 완료

---
+++ frontend/src/components/noticeBox/NoticeBox.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import Icon from '../icon/Icon';
+import { Notice } from '../../config/IconName';
+
+interface NoticeBoxProps {
+  noticeText: string;
+}
+
+const NoticeBoxContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const StyledText = styled.p`
+  font-family: 'Pretendard SemiBold';
+  font-size: 20px;
+  margin-left: 8px;
+`;
+
+const NoticeBox = (props: NoticeBoxProps) => {
+  return (
+    <NoticeBoxContainer>
+      <Icon src={Notice} />
+      <StyledText>{props.noticeText}</StyledText>
+    </NoticeBoxContainer>
+  );
+};
+
+export default NoticeBox;
