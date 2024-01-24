c822eca04214cf12e3fd7476d0014a33f973a32b
tmdwoghd <tmdwoghd@naver.com>
2024-01-24 16:50:28 +0900

    S10P12C207-66 feat: alarmListItem 완료

--- frontend/src/config/IconName.ts
+++ frontend/src/config/IconName.ts
@@ -4,3 +4,5 @@ export const Heart_Purple: string = '/icons/purpleHeart.svg';
 export const Coin: string = '/icons/coin.svg';
 export const Notice: string = '/icons/notice.svg';
 export const Bell: string = '/icons/bell.svg';
+
+export const Alarm: string = '/icons/alarm.svg';
