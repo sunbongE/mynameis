85434709ebd83639fad95ce2eeac0304f7587919
kimahyeon <dhflxhdxhd@gmail.com>
2024-02-01 10:54:11 +0900

    S10P12C207-106 feat: 메인페이지 MainStart, MainReview 모듈
    MainReview 애니메이션 추가 예정

---
+++ frontend/src/modules/mainModules/MainHero.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledMainHeroContainer = styled.div`
+  width: 100%;
+  height: 450px;
+  background-color: #716363;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const MainHero = () => {
+  return <></>;
+};
+
+export default MainHero;
