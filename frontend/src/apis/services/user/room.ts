12d69db12836a140085a89a8e36d30df37cdaaef
kimahyeon <dhflxhdxhd@gmail.com>
2024-02-02 11:08:08 +0900

    S10P12C207-130 feat: 미팅 접속 axios 통신 추가
    는 했는데 아직 확인은 못했어요

--- frontend/src/apis/services/user/room.ts
+++ frontend/src/apis/services/user/room.ts
@@ -1,7 +1,11 @@
 import instance from '../../utils/axiosInstance';
 
 const authUrl = 'couple';
+
 export const getCoupleRoomToken = async (params: { coupleId: number }) => {
-  const response = await instance.post(`${authUrl}/login`, params);
+  console.log('axios 통신 ', params);
+  const response = await instance.get(`${authUrl}/video`, { params });
+
+  console.log('axios 통신 결과', response);
   return response.data;
 };
