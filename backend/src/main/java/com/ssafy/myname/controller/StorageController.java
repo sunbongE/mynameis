5927838a1efb24c7ed6b322bcdf9518869b1464e
10thSSAFY <ssafyiwan@gmail.com>
2024-02-04 16:05:22 +0900

    feat/S10P12C207-133-report: 원터치 신고 시스템 구현
    V2. 출력문 제거

--- backend/src/main/java/com/ssafy/myname/controller/StorageController.java
+++ backend/src/main/java/com/ssafy/myname/controller/StorageController.java
@@ -42,20 +42,19 @@ public class StorageController {
             if (reportId == null) {
                 return new ResponseEntity<>("Failed to upload file", HttpStatus.INTERNAL_SERVER_ERROR);
             }
-            System.out.println("[Firebase에 영상이 업로드 되었습니다.]");  // <===============================================출력문=====================
 
             switch (reportType) {
                 case "욕설":
                 case "성희롱":
                     // 토큰 가져오기
                     String token = auth.getToken();
-                    System.out.println("accessToken: " + token);  // <===============================================출력문=====================
+
                     // 파일 다운로드
                     InputStream in = storageService.downloadFile(reportId);
                     if (in == null) {
                         return new ResponseEntity<>("Failed to download file", HttpStatus.INTERNAL_SERVER_ERROR);
                     }
-                    System.out.println("[Firebase로부터 영상을 읽었습니다. STT 변환을 POST요청 해 보겠습니다.]");  // <===============================================출력문=====================
+
                     // STT 변환 요청
                     PostTranscribe postTranscribe = new PostTranscribe();
                     String transcribeId = postTranscribe.transcribe(token, in);  // InputStream을 직접 전달
@@ -63,10 +62,10 @@ public class StorageController {
                     if (transcribeId == null) {
                         return new ResponseEntity<>("Failed to transcribe file", HttpStatus.INTERNAL_SERVER_ERROR);
                     }
-                    System.out.println("[STT 변환 요청이 완료 되었습니다. 5초 기다립니다.]");  // <===============================================출력문=====================
+
                     // 5초 대기
                     Thread.sleep(5000);
-                    System.out.println("[5초 잘 기다렸습니다. GET 요청을 실행합니다.]");  // <===============================================출력문=====================
+
                     // STT 변환 결과 가져오기
                     GetTranscribe getTranscribe = new GetTranscribe();
                     String allMsgs = null;
@@ -74,10 +73,8 @@ public class StorageController {
                         allMsgs = getTranscribe.getTranscription(transcribeId, token);  // GET 요청을 보내 STT 결과 가져오기
                         Thread.sleep(1000);  // 1초간 대기
                     }
-                    System.out.println("[STT 출력 결과에 따른 필터링 알고리즘을 실행시키겠습니다.]");  // <===============================================출력문=====================
+
                     // 욕설 필터링
-                    //String filterFile = reportType.equals("욕설") ? "src/main/resources/f_words.txt" : "src/main/resources/s_words.txt";
-                    //boolean isProfanityDetected = profanityFilter.filterProfanity(allMsgs, filterFile);
                     String filterFileName = reportType.equals("욕설") ? "/f_words.txt" : "/s_words.txt";
                     InputStream filterFileStream = getClass().getResourceAsStream(filterFileName);
                     boolean isProfanityDetected = profanityFilter.filterProfanity(allMsgs, filterFileStream);
@@ -85,7 +82,6 @@ public class StorageController {
 
                     if (isProfanityDetected) { // 욕설이나 성희롱 단어가 검출되었으면
                         String deleteMessage = storageService.deleteReport(reportId); // 동영상 파일 삭제
-                        System.out.println("[검출 완료 후 파일을 삭제하겠습니다.]");  // <===============================================출력문=====================
                         return new ResponseEntity<>(deleteMessage, HttpStatus.OK);
                     }
 
