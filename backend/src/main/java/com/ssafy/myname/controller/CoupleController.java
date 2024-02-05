3acd6bf6901fe40cb6493f0ed2388350cddbe07a
tmdwoghd <tmdwoghd@naver.com>
2024-02-05 22:01:55 +0900

    S10P12C207-102 : bug 수정

--- backend/src/main/java/com/ssafy/myname/controller/CoupleController.java
+++ backend/src/main/java/com/ssafy/myname/controller/CoupleController.java
@@ -15,7 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
