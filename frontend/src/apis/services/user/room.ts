1698214c9e6af99580ef2840587daeda471b4c59
or_hyn <dhflxhdxhd@gmail.com>
2024-02-16 03:17:55 +0900

    fix: openvidu url

--- frontend/src/apis/services/user/room.ts
+++ frontend/src/apis/services/user/room.ts
@@ -4,8 +4,6 @@ const authUrl = 'couple';
 
 export const getCoupleRoomToken = async (params: { coupleId: number }) => {
   console.log('axios 통신 ', params);
-  // instance.defaults.headers.common['Authorization'] = `Bearer ${sessionStorage.getItem('accessToken')}`;
-  // instance.defaults.headers.common['Content-Type'] = 'application/json';
   const response = await loginInstance.get(`${authUrl}/video`, { params });
 
   console.log('axios 통신 결과', response);
