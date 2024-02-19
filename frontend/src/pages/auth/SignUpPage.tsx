b6b97da127638d332e2aaf78b422e726b061d056
조은서 <eundeok9@gmail.com>
2024-02-19 10:17:00 +0900

    test

--- frontend/src/pages/auth/SignUpPage.tsx
+++ frontend/src/pages/auth/SignUpPage.tsx
@@ -13,6 +13,13 @@ import Footer from '../../components/footer/Footer';
 import { isLoginSelector } from '../../recoil/selectors/isLoginSelector';
 import { useRecoilCallback, useRecoilValue } from 'recoil';
 
+const StyledSignUpPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 const StyledSignUpContainer = styled.div`
   width: 537px;
   height: 1300px;
@@ -39,7 +46,27 @@ const StyledSignUpInputContainer = styled.div`
   line-height: normal;
 `;
 
-const StyleLabel = styled.label`
+const StyledColumnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledColumnContainerGap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const StyledRowContainer = styled.div`
+  display: flex;
+`;
+
+const StyledRowContainerGap = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const StyledLabel = styled.label`
   color: #b0a9a9;
   font-family: 'Plus Jakarta Sans';
   font-size: 12px;
@@ -80,11 +107,10 @@ function SignUp() {
 
   const [coinOpen, setCoinOpen] = useState<boolean>(false);
   const handleCoinPage = () => {
-    setCoinOpen(!coinOpen)
-  }
+    setCoinOpen(!coinOpen);
+  };
 
   const [scrolling, setScrolling] = useState<boolean>(false);
-
 
   const navigate = useNavigate();
   const [passwordConfirm, setPasswordConfirm] = useState('');
@@ -266,49 +292,49 @@ function SignUp() {
 
       const response = await userSignUp({ gender: booleanGender, tags: registrationData.tag, ...restData });
       console.log(registrationData);
-      // console.log('회원가입 성공:', response);
+      console.log('회원가입 성공:', response);
       alert('회원가입 되었습니다. 로그인 페이지로 이동합니다.');
       navigate('/login'); // 로그인 페이지 이동
     } catch (error) {
-      // console.log(registrationData);
-      // console.log('회원가입 실패:', error);
+      console.log(registrationData);
+      console.log('회원가입 실패:', error);
       alert('정보를 입력 안한 부분이 있는지 확인해보세요!');
       // 회원가입 실패 후 처리 : 에러 메시지 표시, 로그인 페이지 이동 등
     }
   };
 
+  const [requestCount, setRequestCount] = useState(0);
+
   const handleBeforePhoneAuth = async () => {
-    setRegistrationData((prevData) => ({ ...prevData, phone: submitPhoneNumber }));
-    console.log(registrationData)
-    // try {
-    //   const response = await userPhoneNumberSubmit({ phoneId: submitPhoneNumber });
-    //   console.log('휴대폰 인증 전송', response, submitPhoneNumber);
-    //   alert('휴대폰 번호를 전송했습니다.');
-    // } catch (error) {
-    //   console.log('휴대폰 인증 전송 안됨', error, submitPhoneNumber);
-    //   alert('휴대폰 인증 전송 실패');
-    // }
+    setRequestCount(requestCount + 1);
+    try {
+      const response = await userPhoneNumberSubmit({ phoneId: submitPhoneNumber });
+      console.log('휴대폰 인증 전송', response, submitPhoneNumber);
+      alert('휴대폰 번호를 전송했습니다.');
+    } catch (error) {
+      console.log('휴대폰 인증 전송 안됨', error, submitPhoneNumber);
+      alert('휴대폰 인증 전송 실패');
+    }
   };
 
   const handleAfterPhoneAuth = async () => {
     try {
-      console.log('아무런 효과가 없습니다.')
-      // const response = await userPhoneAuthentication({ phoneId: submitPhoneNumber, certificationNumber: phoneAuthNumber });
-      // setIsPhoneAuth(true);
-      // setRegistrationData((prevData) => ({ ...prevData, phone: submitPhoneNumber }));
+      const response = await userPhoneAuthentication({ phoneId: submitPhoneNumber, certificationNumber: phoneAuthNumber });
+      setIsPhoneAuth(true);
+      setRegistrationData((prevData) => ({ ...prevData, phone: submitPhoneNumber }));
 
-      // console.log('핸드폰 등록 : ', registrationData.phone);
-      // console.log('휴대폰 인증 성공', response, submitPhoneNumber);
-      // alert('휴대폰 인증에 성공했습니다.');
+      console.log('핸드폰 등록 : ', registrationData.phone);
+      console.log('휴대폰 인증 성공', response, submitPhoneNumber);
+      alert('휴대폰 인증에 성공했습니다.');
     } catch (error) {
-      // console.log('휴대폰 인증 실패', error, submitPhoneNumber);
-      // console.log('핸드폰 등록 : ', registrationData.phone);
-      // alert('휴대폰 인증에 실패했습니다.');
+      console.log('휴대폰 인증 실패', error, submitPhoneNumber);
+      console.log('핸드폰 등록 : ', registrationData.phone);
+      alert('휴대폰 인증에 실패했습니다.');
     }
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+    <StyledSignUpPage>
       <Header
         isLogin={isLogin}
         onClickLogin={handleLogin}
@@ -324,75 +350,74 @@ function SignUp() {
       <StyledSignUpContainer>
         <h2>회원가입</h2>
         <StyledSignUpInputContainer>
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <StyleLabel htmlFor='Id'>아이디</StyleLabel>
+          <StyledColumnContainer>
+            <StyledLabel htmlFor='Id'>아이디</StyledLabel>
             <SimpleInput placeholder='아이디 입력' id='Id' value={registrationData.userId} onInputChange={handleUserIdChange} />
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-            <StyleLabel htmlFor='password'>비밀번호</StyleLabel>
+          </StyledColumnContainer>
+          <StyledColumnContainerGap>
+            <StyledLabel htmlFor='password'>비밀번호</StyledLabel>
             <SimpleInput placeholder='비밀번호 입력' id='password' value={registrationData.password} onInputChange={handlePasswordChange} />
             <PasswordInput placeholder='비밀번호 확인' value={passwordConfirm} onInputChange={handlePasswordConfirmChange} isReturn={true} originValue={registrationData.password} />
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <StyleLabel htmlFor='email'>이메일</StyleLabel>
-            <div style={{ display: 'flex' }}>
+          </StyledColumnContainerGap>
+          <StyledColumnContainer>
+            <StyledLabel htmlFor='email'>이메일</StyledLabel>
+            <StyledRowContainerGap>
               <SimpleInput placeholder='이메일 계정' id='email' value={registrationData.email} onInputChange={handleEmailChange} width='165px' />
-              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '10px' }}>@</div>
+              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>@</div>
               <CustomDropdown options={['이메일 선택', 'gmail.com', 'naver.com', 'hanmail.net', 'kakao.com']} width='103px' onSelected={(value) => handleEmailDropdown(value)} />
-            </div>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-            <StyleLabel htmlFor='username'>이름</StyleLabel>
+            </StyledRowContainerGap>
+          </StyledColumnContainer>
+          <StyledColumnContainerGap>
+            <StyledLabel htmlFor='username'>이름</StyledLabel>
             <SimpleInput placeholder='이름 입력' id='username' value={registrationData.name} onInputChange={handleNameChange} />
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-            <StyleLabel htmlFor='gender'>성별</StyleLabel>
-            <div style={{ display: 'flex' }}>
+          </StyledColumnContainerGap>
+          <StyledColumnContainerGap>
+            <StyledLabel htmlFor='gender'>성별</StyledLabel>
+            <StyledRowContainer>
               <CustomRadioButton values={genderValues} version='gender' selected={selectedGender} setSelected={setSelectedGender} onSelected={handleGenderChange} />
-            </div>
-          </div>
-          <div>
-            <StyleLabel htmlFor='birth'>생년월일</StyleLabel>
-            <div style={{ display: 'flex', gap: '8px' }}>
-              <CustomDropdown options={birthYearOptions} width='94px' onSelected={(value) => handleBirthDropdownChange(value, 'year')} />
-              <CustomDropdown options={birthMonthOptions} width='94px' onSelected={(value) => handleBirthDropdownChange(value, 'month')} />
-              <CustomDropdown options={birthDayOptions} width='94px' onSelected={(value) => handleBirthDropdownChange(value, 'day')} />
-            </div>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-            <StyleLabel htmlFor='phoneAuth'>휴대폰 인증</StyleLabel>
-            <div style={{ display: 'flex', gap: '10px' }}>
+            </StyledRowContainer>
+          </StyledColumnContainerGap>
+          <StyledLabel htmlFor='birth'>생년월일</StyledLabel>
+          <StyledRowContainerGap>
+            <CustomDropdown options={birthYearOptions} width='94px' onSelected={(value) => handleBirthDropdownChange(value, 'year')} />
+            <CustomDropdown options={birthMonthOptions} width='94px' onSelected={(value) => handleBirthDropdownChange(value, 'month')} />
+            <CustomDropdown options={birthDayOptions} width='94px' onSelected={(value) => handleBirthDropdownChange(value, 'day')} />
+          </StyledRowContainerGap>
+          <StyledColumnContainerGap>
+            <StyledLabel htmlFor='phoneAuth'>휴대폰 인증</StyledLabel>
+            <StyledRowContainerGap>
               <SimpleInput placeholder='전화번호를 입력하세요' id='phoneAuth' value={registrationData.phone} onInputChange={handlePhoneChange} width='220px' />
-              <Button width='70px' height='50px' $borderRadius='10px' $backgroundColor='#E1A4B4' $fontColor='#FFF' $fontSize='12px' onButtonClick={handleBeforePhoneAuth} $children={"다시 요청"} />
-            </div>
+
+              <Button width='70px' height='50px' $borderRadius='10px' $backgroundColor='#E1A4B4' $fontColor='#FFF' $fontSize='12px' onButtonClick={handleBeforePhoneAuth} $children={requestCount ? '재요청' : '요청'} />
+            </StyledRowContainerGap>
             <ConfirmationCodeInput placeholder='인증번호를 입력하고 엔터를 눌러주세요' value={phoneAuthNumber} onInputChange={handlePhoneAuthChange} onEnterKeyUp={handleAfterPhoneAuth} />
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <StyleLabel htmlFor='area'>지역</StyleLabel>
+          </StyledColumnContainerGap>
+          <StyledColumnContainer>
+            <StyledLabel htmlFor='area'>지역</StyledLabel>
             <CustomDropdown options={administrativeDistrict} width='300px' onSelected={(value) => handleAreaChange(value, 'area')} />
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <StyleLabel htmlFor='job'>직업</StyleLabel>
+          </StyledColumnContainer>
+          <StyledColumnContainer>
+            <StyledLabel htmlFor='job'>직업</StyledLabel>
             <SimpleInput placeholder='직업을 입력하세요' id='job' value={registrationData.job} onInputChange={handleJobChange} />
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <StyleLabel htmlFor='hashtag'>MBTI/흥미/특기</StyleLabel>
-            <SimpleInput placeholder='나를 표현하는 단어를 입력하세요' id='hashtag' value={tagItem} onInputChange={handleTagChange} onEnterKeyUp={(value) => handleChipCreation(value)} />
+          </StyledColumnContainer>
+          <StyledColumnContainer>
+            <StyledLabel htmlFor='hashtag'>MBTI/흥미/특기</StyledLabel>
+            <SimpleInput placeholder='나를 표현하는 단어를 입력하고 엔터를 눌러주세요' id='hashtag' value={tagItem} onInputChange={handleTagChange} onEnterKeyUp={(value) => handleChipCreation(value)} />
             <div style={{ display: 'flex', marginTop: '8px' }}>
               {registrationData.tag.map((tagItem, index) => (
                 <Chip keyword={tagItem} key={index} id={index} onDeleteItem={onDeleteTag} />
               ))}
             </div>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <StyleLabel htmlFor='religion'>종교</StyleLabel>
+          </StyledColumnContainer>
+          <StyledColumnContainer>
+            <StyledLabel htmlFor='religion'>종교</StyledLabel>
             <CustomDropdown options={['종교를 선택하세요', '무교', '기독교', '천주교', '불교']} width='300px' onSelected={(value) => handleReligionChange(value, 'religion')} />
-          </div>
-          <Button width='300px' height='50px' $borderRadius='10px' $backgroundColor='#E1A4B4' $fontColor='#FFF' onButtonClick={handleSignUp} $children={"입력 완료"} />
+          </StyledColumnContainer>
+          <Button width='300px' height='50px' $borderRadius='10px' $backgroundColor='#E1A4B4' $fontColor='#FFF' onButtonClick={isPhoneAuth ? handleSignUp : undefined} disabled={!isPhoneAuth} $children={'입력 완료'} />
         </StyledSignUpInputContainer>
       </StyledSignUpContainer>
       <Footer />
-    </div>
+    </StyledSignUpPage>
   );
 }
 
