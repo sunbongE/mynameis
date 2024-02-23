8e891101fa84d12e2a4067362d7e656dbeba55de
박태호 <qkrxogh7@naver.com>
2024-02-23 09:35:22 +0900

    registerStompRegistry allowedOrigins 수정

--- backend/src/main/java/com/ssafy/myname/config/WebSockConfig.java
+++ backend/src/main/java/com/ssafy/myname/config/WebSockConfig.java
@@ -26,16 +26,18 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/stomp")
-                .setAllowedOrigins(
-                        "http://localhost:3000", "http://localhost:8081",
-                        "https://i10c207.p.ssafy.io", "https://mynameis.site")
-                .setAllowedOriginPatterns(
-                        "http://localhost:3000/**", "http://localhost:8081/**",
-                        "https://i10c207.p.ssafy.io/**", "https://mynameis.site/**")
-                .withSockJS()
-                .setDisconnectDelay(30 * 1000)
-                .setClientLibraryUrl(
-                        "https://cdnjs.cloudflare.com/ajax/libs/sockjs-client/1.5.4/sockjs.min.js");
+                .setAllowedOrigins("*");
+//
+//                .setAllowedOrigins(
+//                        "http://localhost:3000", "http://localhost:8081",
+//                        "https://i10c207.p.ssafy.io", "https://mynameis.site")
+//                .setAllowedOriginPatterns(
+//                        "http://localhost:3000/**", "http://localhost:8081/**",
+//                        "https://i10c207.p.ssafy.io/**", "https://mynameis.site/**")
+//                .withSockJS()
+//                .setDisconnectDelay(30 * 1000)
+//                .setClientLibraryUrl(
+//                        "https://cdnjs.cloudflare.com/ajax/libs/sockjs-client/1.5.4/sockjs.min.js");
 
     }
 
